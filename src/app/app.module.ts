d7b1847310078b68ab323030a0333e9408e95ecd
Freddy <freddy.artea@gmail.com>
2019-09-02 22:10:28 -0500

    service to control action of chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+
+
 import { AngularFirestoreModule, AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
 
+// Components
+import { ChatComponent } from './components/chat/chat.component';
+
+// Services
+import { ChatService } from './providers/chat.service';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
-    AngularFireStorageModule // imports firebase/storage only needed for storage features
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    FormsModule
 
   ],
-  providers: [AngularFirestore],
+  providers: [
+    AngularFirestore,
+    ChatService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
