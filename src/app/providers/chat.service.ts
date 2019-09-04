50603daa16fa2f2a45dcbb67da3488e53fa814f7
Freddy <freddy.artea@gmail.com>
2019-09-04 12:42:34 -0500

    upload and add mesages to firebase

--- src/app/providers/chat.service.ts
+++ src/app/providers/chat.service.ts
@@ -1,22 +1,42 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 
+import { Mensaje } from '../interface/mensaje.interface';
+import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  public chats: any[] = []
+  public chats: Mensaje[] = []
 
-  private itemsCollection: AngularFirestoreCollection<any>;
+  private itemsCollection: AngularFirestoreCollection<Mensaje>;
 
   constructor( private afs: AngularFirestore ) { }
 
   cargarMensajes() {
-    this.itemsCollection = this.afs.collection<any>('chats');
+    this.itemsCollection = this.afs.collection<Mensaje>('chats');
 
-    return this.itemsCollection.valueChanges();
+    return this.itemsCollection.valueChanges().pipe(
+                               map( (mensajes: Mensaje[]) => {
+                                  console.log(mensajes);
+                                  this.chats = mensajes;                             
+                              } ))
+
+  }
+
+  agregarMensaje( text: string ) {
+
+    //TODO falta el UID del usuario
+    let mensaje: Mensaje = {
+      nombre : 'Demo',
+      mensaje: text,
+      fecha: new Date().getTime()
+    }
+
+    return this.itemsCollection.add( mensaje );
 
   }
 }
